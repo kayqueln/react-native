54826486999239c6f10ad15d11b417738125b0ea
kayqueln <kayqueln@gmail.com>
2024-02-09 22:32:04 -0300

    hello world and button

--- ReactNative/App.js
+++ ReactNative/App.js
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={{ color: 'white', fontSize: 25}}>Boa noite, Bruno!</Text>
+      <Button title="Clica aqui mano"/>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,7 +14,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#000',
     alignItems: 'center',
     justifyContent: 'center',
   },
